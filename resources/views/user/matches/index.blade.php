6f5fb2226e688d2e15243ce9507f5dbe14b8eebe
aoifeb164 <n00182228@student.iadt.ie>
2021-05-10 17:51:51 +0100

    buttons

--- resources/views/user/matches/index.blade.php
+++ resources/views/user/matches/index.blade.php
@@ -84,11 +84,11 @@
                                                 </a></td>
                                             <td>{{ $profile->dob }}</td>
                                             <td>{{ $profile->sign->title }}</td>
-                                            <td><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><path fill="#08a121" d="M253.924 127.592a64 64 0 1 0 64 64a64.073 64.073 0 0 0-64-64zm0 96a32 32 0 1 1 32-32a32.037 32.037 0 0 1-32 32z"/><path fill="#08a121" d="M376.906 68.515A173.922 173.922 0 0 0 108.2 286.426l120.907 185.613a29.619 29.619 0 0 0 49.635 0l120.911-185.613a173.921 173.921 0 0 0-22.747-217.911zm-4.065 200.444l-118.916 182.55l-118.917-182.55c-36.4-55.879-28.593-130.659 18.563-177.817a141.92 141.92 0 0 1 200.708 0c47.156 47.158 54.962 121.938 18.562 177.817z"/></svg>{{ $profile->location }}</td>
+                                            <td><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="-1 -5 18 25"><path fill="#08a121" d="M253.924 127.592a64 64 0 1 0 64 64a64.073 64.073 0 0 0-64-64zm0 96a32 32 0 1 1 32-32a32.037 32.037 0 0 1-32 32z"/><path fill="#08a121" d="M376.906 68.515A173.922 173.922 0 0 0 108.2 286.426l120.907 185.613a29.619 29.619 0 0 0 49.635 0l120.911-185.613a173.921 173.921 0 0 0-22.747-217.911zm-4.065 200.444l-118.916 182.55l-118.917-182.55c-36.4-55.879-28.593-130.659 18.563-177.817a141.92 141.92 0 0 1 200.708 0c47.156 47.158 54.962 121.938 18.562 177.817z"/></svg>{{ $profile->location }}</td>
                                             <td>{{ $profile->pivot->status }}</td>
                                             <td>
                                               <a href="{{ route('user.conversations.create') }}" class"btn btn-outline">
-                                                <button type="submit" id="accept" class="btn btn-outline-primary" style="border-radius:100px; margin:10px;"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#5d90e3" class="bi bi-envelope" viewBox="0 0 16 16">
+                                                <button type="submit" id="message" class="btn btn-outline-primary" style="border-radius:100px; margin:10px;"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-envelope" viewBox="-1 -5 18 25">
                                                   <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
                                                 </svg></button>
                                               </a>
@@ -96,7 +96,7 @@
                                                 <form style="display:inline-block" method="POST" action="{{ route('user.matches.destroy', $profile->id ) }}">
                                                     <input type="hidden" name="_method" value="DELETE">
                                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                    <button type="submit" id="accept" class="btn btn-outline-danger" style="border-radius:100px; margin:10px;"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><path d="M473.7 73.8l-2.4-2.5c-46-47-118-51.7-169.6-14.8L336 159.9l-96 64l48 128l-144-144l96-64l-28.6-86.5C159.7 19.6 87 24 40.7 71.4l-2.4 2.4C-10.4 123.6-12.5 202.9 31 256l212.1 218.6c7.1 7.3 18.6 7.3 25.7 0L481 255.9c43.5-53 41.4-132.3-7.3-182.1z" fill="#e81c1c"/></svg></a>
+                                                    <button type="submit" id="delete" class="btn btn-outline-danger" style="border-radius:100px; margin:10px;"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="broken-heart" viewBox="-1 -5 18 25"> <svg xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><path d="M473.7 73.8l-2.4-2.5c-46-47-118-51.7-169.6-14.8L336 159.9l-96 64l48 128l-144-144l96-64l-28.6-86.5C159.7 19.6 87 24 40.7 71.4l-2.4 2.4C-10.4 123.6-12.5 202.9 31 256l212.1 218.6c7.1 7.3 18.6 7.3 25.7 0L481 255.9c43.5-53 41.4-132.3-7.3-182.1z"/></svg></a>
                                                 </form>
 
 
@@ -151,7 +151,7 @@
 
                                                 <input type="hidden" name="status" id="match-status" value="" />
                                                 <button type="submit" id="accept" class="btn btn-outline-success" style="border-radius:100px; margin:10px;" formaction="{{route('user.matches.accept')}}">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart-fill" viewBox="-1 -5 18 25">
                                                         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
                                                     </svg>
                                                 </button>
@@ -178,7 +178,7 @@
                                                   <td>{{ $profile->pivot->status }}</td>
                                                   <td>
                                               <a href="{{ route('user.conversations.create') }}" class"btn btn-outline">
-                                                <button type="submit" id="accept" class="btn btn-outline-primary" style="border-radius:100px; margin:10px;"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#5d90e3" class="bi bi-envelope" viewBox="0 0 16 16">
+                                                <button type="submit" id="message" class="btn btn-outline-primary" style="border-radius:100px; margin:10px;"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-envelope" viewBox="-1 -5 18 25">
                                                   <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
                                                 </svg></button>
                                               </a>
@@ -186,7 +186,7 @@
                                                 <form style="display:inline-block" method="POST" action="{{ route('user.matches.destroy', $profile->id ) }}">
                                                     <input type="hidden" name="_method" value="DELETE">
                                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                    <button type="submit" id="accept" class="btn btn-outline-danger" style="border-radius:100px; margin:10px;"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><path d="M473.7 73.8l-2.4-2.5c-46-47-118-51.7-169.6-14.8L336 159.9l-96 64l48 128l-144-144l96-64l-28.6-86.5C159.7 19.6 87 24 40.7 71.4l-2.4 2.4C-10.4 123.6-12.5 202.9 31 256l212.1 218.6c7.1 7.3 18.6 7.3 25.7 0L481 255.9c43.5-53 41.4-132.3-7.3-182.1z" fill="#e81c1c"/></svg></a>
+                                                    <button type="submit" id="delete" class="btn btn-outline-danger" style="border-radius:100px; margin:10px;"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="broken-heart" viewBox="-1 -5 18 25"> <svg xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><path d="M473.7 73.8l-2.4-2.5c-46-47-118-51.7-169.6-14.8L336 159.9l-96 64l48 128l-144-144l96-64l-28.6-86.5C159.7 19.6 87 24 40.7 71.4l-2.4 2.4C-10.4 123.6-12.5 202.9 31 256l212.1 218.6c7.1 7.3 18.6 7.3 25.7 0L481 255.9c43.5-53 41.4-132.3-7.3-182.1z"/></svg></a>
                                                 </form>
 
                                                 @endif
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,82 +5,84 @@
     <div class="row justify-content-center">
         <div class="col-md-8" style="padding: 0px; padding-left:120px">
 
-              <div class="header" style="padding-left:35px;">
+            <div class="header" style="padding-left:35px;">
                 <h2 style=" padding-top:60px;">{{ __('Welcome') }}</h2>
                 <h2>{{ __('Back') }}</h2>
-                </div>
+            </div>
 
-                <div class="card-body" style="padding-bottom:0px; padding-right:0px;">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+            <div class="card-body" style="padding-bottom:0px; padding-right:0px;">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
 
-                        <div class="form">
-                          <div class="mb-3">
+                    <div class="form">
+                        <div class="mb-3">
                             <label for="email" class="col-md-4 col-form-label">{{ __('Email Address') }}</label>
 
                             <div class="col-md-10">
-                                <input id="email" type="email" placeholder="name@example.com" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" placeholder="name@example.com" class="form-control
+                                @error('email') is-invalid
+                                @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
-                          </div>
                         </div>
+                    </div>
 
-                        <div class="form">
-                            <div class="mb-3">
+                    <div class="form">
+                        <div class="mb-3">
                             <label for="password" class="col-md-4 col-form-label">{{ __('Password') }}</label>
 
                             <div class="col-md-10">
                                 <input id="password" type="password" placeholder="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
-                          </div>
                         </div>
+                    </div>
 
-                        {{-- <div class="form-group row">
+                    {{-- <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div> --}}
-                        <div class="col-md-10 text-right">
-                            @if (Route::has('password.request'))
-                                <a class="btn btn-link" style="color:#35144f;" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Password?') }}
-                                </a>
-                            @endif
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-9 text-right" style="margin-bottom:0px;">
-                                <button type="submit" class="btn">
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="purple" class="bi bi-arrow-right" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-</svg>
-                                </button>
-                            </div>
-                              </div>
-                    </form>
-  </div>
-
+                    <label class="form-check-label" for="remember">
+                        {{ __('Remember Me') }}
+                    </label>
+            </div>
         </div>
-        <div class="col-md-2" style="padding: 0px">
-            <img src="/images/home/image.PNG" height="300" width="420" style="margin-top:370px;">
-          </div>
+    </div> --}}
+    <div class="col-md-10 text-right">
+        @if (Route::has('password.request'))
+        <a class="btn btn-link" style="color:#35144f;" href="{{ route('password.request') }}">
+            {{ __('Forgot Password?') }}
+        </a>
+        @endif
     </div>
+
+    <div class="form-group row mb-0">
+        <div class="col-md-9 text-right" style="margin-bottom:0px;">
+            <button type="submit" class="btn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="purple" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                </svg>
+            </button>
+        </div>
+    </div>
+    </form>
+</div>
+
+</div>
+<div class="col-md-2" style="padding: 0px">
+    <img src="/images/home/image.PNG" height="300" width="420" style="margin-top:370px;">
+</div>
+</div>
 </div>
 @endsection
